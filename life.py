edf9df96d687cbdaeb27b95e87f1d33380d0b35f
Ayushman Roy <ayushman.roy1998@gmail.com>
2024-07-28 01:50:25 +0530

    First Upload of All files

---
+++ life.py
@@ -0,0 +1,9 @@
+import math
+
+theAnswer = 42
+
+def square_root(input):
+    return math.sqrt(input)
+
+def quote_marvin():
+    print('I\'ve calculated your chance of survival, but I don\'t think you\'ll like it.')
